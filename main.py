1f904a9b1498135358899c62a9162456d3b46624
Astaroth <astarothphone@gmail.com>
2026-09-22 16:34:09 +0300

    -

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from core.application import Application
+import os
+os.environ.setdefault("GLOG_minloglevel", "2")
 
+from core.application import Application
 
 if __name__ == "__main__":
     Application().run()
-2
